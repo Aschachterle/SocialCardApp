86bca679ff42576e7f35eacc7542a6d694ce122a
Adam <adam.schachterle@gmail.com>
2021-05-31 20:34:17 -0400

    added dynamic post images

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Card from './components/card'
 function App() {
   
   let socialData = [
-    { poster: "Adam Schachterle", cardtext: "Woah cool, look at this!", posttitle: "Computers are fun", posttitlesubtext: "Do you like computers, well how about them computers?", key: 1 },
+    { poster: "Adam Schachterle", cardtext: "Woah cool, look at this!", posttitle: "Computers are fun", posttitlesubtext: "Do you like computers, well how about them computers?", key: 1, img: 'url(https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8xOTYwMDA3Ny9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY2NjEwODk4OH0.Ak820-nwF0IHPksLKc2pA3Bkby4Un2spz0PnZkzn_90/img.jpg?width=1245&quality=85&coordinates=0%2C460%2C0%2C165&height=700)' },
 
-    { poster: "Annmarie Scaglione", cardtext: "I like ponies!", posttitle: "Ponies are cool and I really like them!", posttitlesubtext: "Check out these purple people eating ponies", key: 2 },
+    { poster: "Annmarie Scaglione", cardtext: "I like ponies!", posttitle: "Ponies are cool and I really like them!", posttitlesubtext: "Check out these purple people eating ponies", key: 2, img: 'url(https://horse-canada.com/wp-content/uploads/2020/07/Shetland-Pony-web.jpg)'},
 
-    { poster: "Erik Schachterle", cardtext: "More Hotrods", posttitle: "Hotrods are super popular right now", posttitlesubtext: "Find out how to save money on hotrod parts", key: 3 }
+    { poster: "Erik Schachterle", cardtext: "More Hotrods", posttitle: "Hotrods are super popular right now", posttitlesubtext: "Find out how to save money on hotrod parts", key: 3, img: 'url(https://i2.wp.com/motoringartist.com/wp-content/uploads/2015/11/016-Saltflats-e1605627451459.jpg?fit=750%2C511)' }
   ]
   console.log(socialData[0].poster)
 
--- src/components/post.js
+++ src/components/post.js
@@ -9,7 +9,7 @@ const Post = (props) => {
   return (
     <>
         <div className="link__card">
-            <div className="post__img"></div>
+            <div className="post__img" style={{backgroundImage: props.value.img}}></div>
             <div className="link__description__box">
                 <div className="link__description">{props.value.posttitle}</div>
                 <div className="link__description__subtext">{props.value.posttitlesubtext}</div>
